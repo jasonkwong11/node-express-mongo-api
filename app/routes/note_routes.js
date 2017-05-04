340faa48da5bab7585a70477e3c34cd7f8983dd7
Jason <jasonkwong11@gmail.com>
2017-05-04 01:28:33 -0400

    added update and destroy routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -14,6 +14,18 @@ module.exports = function(app, db) {
     });
   });
 
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send('Note' + id + 'has been deleted!');
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
@@ -23,5 +35,19 @@ module.exports = function(app, db) {
         res.send(result.ops[0]);
       }
     });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { 'text': req.body.body, title: req.body.title };
+
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(note);
+      }
+    })
   });
 };
